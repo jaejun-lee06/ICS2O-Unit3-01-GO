dd8a6d14f5a493d5b5606d7932dc45dd3bd4bedc
jaejun-lee06 <99355321+jaejun-lee06@users.noreply.github.com>
2022-03-31 01:30:51 +0000

    done

--- main.go
+++ main.go
@@ -1,3 +1,7 @@
+// Created by: Jaejun Lee
+// Created on: Mar 2022
+//
+
 package main
 
 import (
@@ -5,5 +9,22 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	// This function does addition
+	var aBase int
+	var bBase int
+	var height int
+
+	// input
+	fmt.Println("This program finds the area of a trapezoid")
+	fmt.Println()
+	fmt.Println("Enter the a base (cm): ")
+	fmt.Scanln(&aBase)
+	fmt.Println("Enter the b base (cm): ")
+	fmt.Scanln(&bBase)
+	fmt.Println("Enter the height (cm): ")
+	fmt.Scanln(&height)
+
+	// output
+	fmt.Println("The area is: ", ((aBase+bBase)/2)*height, "cm²")
+	fmt.Println("Done.")
 }
